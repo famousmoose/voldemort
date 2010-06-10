63815363e27a8728138d06daf5e0f4386d025db0
Stu Teasdale <stu@drogna.org.uk>
2010-06-10 12:03:25 +0100

    Return a throughput of 0 if no time has passed since the accumulator was created.

--- src/java/voldemort/store/stats/RequestCounter.java
+++ src/java/voldemort/store/stats/RequestCounter.java
@@ -39,7 +39,7 @@ public class RequestCounter {
         if(elapsed > 0f) {
             return (float) (oldv.count / elapsed);
         } else {
-            return -1f;
+            return 0f;
         }
     }
 
